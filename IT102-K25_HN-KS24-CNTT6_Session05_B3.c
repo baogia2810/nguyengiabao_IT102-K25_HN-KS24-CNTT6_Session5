ed2d2a422293afdbd9a1e55f1ded7c58f17f71a7
baogia2810 <bankaijun2880@gmail.com>
2025-08-01 08:51:29 +0700

    upload bài 3

---
+++ IT102-K25_HN-KS24-CNTT6_Session05_B3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, x;
+
+    printf("Nhap he so a: ");
+    scanf("%f", &a);
+    printf("Nhap he so b: ");
+    scanf("%f", &b);
+
+    if (a == 0 && b == 0) {
+        printf("Phuong trinh co vo so nghiem\n");
+    } else if (a == 0) {
+        printf("Phuong trinh vo nghiem\n");
+    } else {
+        x = -b / a;
+        printf("Nghiem cua phuong trinh la x = %.2f\n", x);
+    }
+
+    return 0;
+}
